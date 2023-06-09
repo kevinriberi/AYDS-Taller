e2505b4646751636eab889842deedaa52632120a
Kevin <kevinriberi@gmail.com>
2023-06-09 20:18:50 -0300

    Ahora se muestran preguntas al azar del nivel correspondiente al usuario

--- AppWeb/controllers/topic_controller.rb
+++ AppWeb/controllers/topic_controller.rb
@@ -23,11 +23,16 @@ class TopicController < Sinatra::Application
       get '/topics/:id' do
         # Obtener el ID del tema seleccionado
         topic_id = params[:id]
+        user_id = session[:user_id]
         
-        # Obtener la primera pregunta del tema seleccionado
-        first_question = Question.where(topic_id: topic_id).first
+        # Obtener una pregunta aleatoria del tema seleccionado que se corresponda
+        # con el nivel del usuario
+        user_info = Knowledge.find_by(user_id: user_id, topic_id: topic_id)
+        level = user_info.level
+        questions = Question.where(topic_id: topic_id, level: level).order("RANDOM()")
+        question = questions.sample
         
-        # Redirigir al usuario a la página de la primera pregunta
-        redirect "/questions/#{first_question.id}/answer"
+        # Redirigir al usuario a la página de la pregunta seleccionada al azar
+        redirect "/questions/#{question.id}/answer"
       end
 end
--- AppWeb/models/question.rb
+++ AppWeb/models/question.rb
@@ -2,5 +2,4 @@ class Question < ActiveRecord::Base
     belongs_to :topic
     has_many :options
     belongs_to :correct_option, class_name: "Option"
-
 end
