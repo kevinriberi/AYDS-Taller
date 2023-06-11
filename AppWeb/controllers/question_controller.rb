6bcd9026b742df8256537d3ac636cc51dab1dd9f
Kevin <kevinriberi@gmail.com>
2023-06-11 00:22:01 -0300

    Se agregó la lógica para cuando el usuario completa un tema

--- AppWeb/controllers/question_controller.rb
+++ AppWeb/controllers/question_controller.rb
@@ -1,62 +1,19 @@
 require_relative '../models/answer'
 
 class QuestionController < Sinatra::Application
-
-    # Ruta para mostrar todas las preguntas
-    get '/questions' do
-      @questions = Question.all
-      @topics = Topic.all
-
-      erb :'question/questions'
-    end
   
-    # Ruta para mostrar el formulario de nueva pregunta
-    get '/questions/new' do
-      @question = Question.new
-      @topics = Topic.all
+  # Ruta para mostrar una pregunta individual
+  get '/questions/:id' do
+    @question = Question.find(params[:id])
+    @options = @question.options
 
-      erb :'question/new_question'
-    end
-  
-    # Ruta para crear una nueva pregunta
-    post '/questions' do
-      # crea una nueva pregunta
-      question = Question.create(content: params[:question], topic_id: params[:topic], level: params[:level] )
-    
-      # crea las opciones de la pregunta
-      option1 = Option.create(content: params[:option1])
-      option2 = Option.create(content: params[:option2])
-      option3 = Option.create(content: params[:option3])
-    
-      # actualiza la opción correcta de la pregunta
-      if params[:correct_option] == '1'
-        question.update(correct_option_id: option1.id)
-      elsif params[:correct_option] == '2'
-        question.update(correct_option_id: option2.id)
-      elsif params[:correct_option] == '3'
-        question.update(correct_option_id: option3.id)
-      end
-    
-      option1.update(question_id: question.id)
-      option2.update(question_id: question.id)
-      option3.update(question_id: question.id)
-      # redirige a la página de todas las preguntas
-      redirect '/questions/new'
-    end
-  
-    # Ruta para mostrar una pregunta individual
-    get '/questions/:id' do
-      @question = Question.find(params[:id])
-      @options = @question.options
+    erb :'question/question'
+  end
 
-      erb :'question/question'
-    end
+  # Ruta para mostrar la pregunta individual y su formulario de respuesta
+  get '/questions/:id/answer' do
+    @question = Question.find(params[:id])
 
-    # Ruta para mostrar la pregunta individual y su formulario de respuesta
-    get '/questions/:id/answer' do
-      @question = Question.find(params[:id])
-
-      erb :'answer'
-    end
-
+    erb :'answer'
+  end
 end
--- AppWeb/controllers/topic_controller.rb
+++ AppWeb/controllers/topic_controller.rb
@@ -1,38 +1,23 @@
 class TopicController < Sinatra::Application
-    get '/topics' do        
-        @topics = Topic.all
-        
-        erb :'topic/topics'
-      end
-    
-      get '/topics/new' do
-        @topic = Topic.new
 
-        erb :'topic/new_topic'
-      end
-    
-      post '/topics' do
-        @topic = Topic.new(params[:topic])
-        if @topic.save
-          redirect '/topics'
-        else
-          erb :'topic/new_topic'
-        end
-      end
-    
-      get '/topics/:id' do
-        # Obtener el ID del tema seleccionado
-        topic_id = params[:id]
-        user_id = session[:user_id]
+  get '/topics/:id' do
+    # Obtener el ID del tema seleccionado
+    topic_id = params[:id]
+    user_id = session[:user_id]
         
-        # Obtener una pregunta aleatoria del tema seleccionado que se corresponda
-        # con el nivel del usuario
-        user_info = Knowledge.find_by(user_id: user_id, topic_id: topic_id)
-        level = user_info.level
-        questions = Question.where(topic_id: topic_id, level: level).order("RANDOM()")
-        question = questions.sample
-        
-        # Redirigir al usuario a la página de la pregunta seleccionada al azar
-        redirect "/questions/#{question.id}/answer"
-      end
+    # Obtener una pregunta aleatoria del tema seleccionado que se corresponda
+    # con el nivel del usuario
+    user_info = Knowledge.find_by(user_id: user_id, topic_id: topic_id)
+    level = user_info.level
+    questions = Question.where(topic_id: topic_id, level: level).order("RANDOM()")
+    question = questions.sample
+      
+    # Redirigir al usuario a la página de la pregunta seleccionada al azar
+    redirect "/questions/#{question.id}/answer"
+  end
+
+    # Obtener las preguntas que aún no ha respondido correctamente
+    answered_questions = user_info.knowledge_questions.where(correct: true).pluck(:question_id)
+    questions = Question.where(topic_id: topic_id, level: level).where.not(id: answered_questions).order("RANDOM()")
+    question = questions.sample
 end
