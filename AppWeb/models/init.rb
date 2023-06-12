98ec84241755cd75e5f0ad0c96a46b4bad2cae84
Kevin <kevinriberi@gmail.com>
2023-06-12 20:21:59 -0300

    Gemfile actualizado

--- AppWeb/models/init.rb
+++ AppWeb/models/init.rb
@@ -1,3 +1,4 @@
+require 'active_record'
 require_relative 'user'
 require_relative 'question'
 require_relative 'option'
---
+++ AppWeb/spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+# spec/models/user_spec.rb
+require_relative '../../models/init.rb'
+
+describe 'User' do
+  describe 'valid' do
+    describe 'when there is no name' do
+      it 'should be invalid' do
+        u = User.new
+        expect(u.valid?).to eq(false)
+      end
+    end
+  end
+end
