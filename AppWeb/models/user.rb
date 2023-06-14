db627c0cb5cbe3778d2da509c1decaa566a65b89
Kevin <kevinriberi@gmail.com>
2023-06-13 21:46:29 -0300

    Modificacion en el modelo User para que pasen los tests

--- AppWeb/models/user.rb
+++ AppWeb/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
     has_many :answers
     has_many :knowledges
     has_many :topics, :through => :knowledges
+
+    validates :username, presence: true
+    validates :email, presence: true, uniqueness: true
 end
--- AppWeb/spec/models/user_spec.rb
+++ AppWeb/spec/models/user_spec.rb
@@ -4,11 +4,29 @@ require_relative '../../models/init.rb'
 
 
 describe User do
-  it "is valid with a firstname, lastname and email"
+  it "is valid with a username and email" do
+    user = User.new(username: "Martincito28", email: "martinp@example.com", password_digest: "password")
+    expect(user).to be_valid
+  end
 
-  it "is invalid without a firstname"
-  it "is invalid without a lastname"
-  it "is invalid without an email address"
-  it "is invalid with a duplicate email address"
-  it "returns a contact's full name as a string"
+  it "is invalid without a username" do
+    user = User.new(email: "roman10@example.com", password_digest: "password")
+    expect(user).not_to be_valid
+  end
+
+  it "is invalid without an email address" do
+    user = User.new(username: "RR10", password_digest: "password")
+    expect(user).not_to be_valid
+  end
+
+  it "is invalid without an password" do
+    user = User.new(username: "RR10", email: "roman10@gmail.com")
+    expect(user).not_to be_valid
+  end
+
+  it "is invalid with a duplicate email address" do
+    existing_user = User.create(username: "Messi10", email: "messi@example.com", password_digest: "password")
+    user = User.new(username: "argento", email: "messi@example.com", password_digest: "password")
+    expect(user).not_to be_valid
+  end
 end
